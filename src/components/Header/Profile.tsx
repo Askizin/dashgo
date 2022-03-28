ac4c4933b1b1b5f8e5ec512e786ad1c139a7541a
iury <iury_barbosa07@hotmail.com>
2022-03-28 04:22:44 -0300

    responsividade aplicada no header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Iury Barbosa</Text>
-                <Text color="gray.300" fontSize="small">Iury_barbosa07@hotmail.com</Text>
-            </Box>
-        
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Iury Barbosa</Text>
+                    <Text color="gray.300" fontSize="small">Iury_barbosa07@hotmail.com</Text>
+                </Box>
+            )}
 
-        <Avatar size="md" name="Iury Barbosa" src="https://github.com/askizin.png"/>
+
+            <Avatar size="md" name="Iury Barbosa" src="https://github.com/askizin.png" />
         </Flex>
     );
 }
